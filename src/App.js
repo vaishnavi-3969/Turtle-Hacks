83c1144a549fee92510ffd3ec99bcb8cbf4e12ee
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-14 17:21:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Banner } from './components/Banner';
+import { Pastwork } from './components/Pastwork';
+import { Initiatives } from './components/Initiatives';
+import { ContactForm } from './components/ContactForm';
+import { Footer } from './components/Footer';
+import ScrollToTopButton from './components/ScrollToTopButton';
+import { Volunteer } from './components/Volunteer';
+import { OrganizeFoodDrive } from './components/OrganizeDrive';
+import DonateFunds from './components/DonateFunds';
+import DonateFoodKits from './components/DonateKits';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar/>
+    <Banner/>
+    {/* <Chat/> */}
+    <Pastwork/>
+    <Initiatives/>
+    <Volunteer/>
+    <OrganizeFoodDrive/>
+    <DonateFunds/>
+    <DonateFoodKits/>
+    <ContactForm/>
+    <Footer/>
+    <ScrollToTopButton/>
     </div>
   );
 }
